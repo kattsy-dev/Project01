55ead0e873420b4d8694fec168aa974aaf5abf74
mrnotepad <krislane.santillan@gmail.com>
2024-12-16 03:39:38 +0800

    Updated the Interface

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,49 @@ import 'package:flutter/material.dart';
 //void main() => runApp(const MyApp());
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: false),
-    home: Scaffold(
+  runApp(
+      MaterialApp(theme: ThemeData(useMaterial3: false), home: const Home()));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         //properties: value
-        title: const Text("Hello World Test"),
+        title: const Text(
+          "Hello World Test",
+        ),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: const Center(
-        child: Text("Hello Ninjas"),
+        child: Image(
+          image: AssetImage('assets/space1.jpg'),
+          // other way to write it
+          // child: Image.asset('assets/space1.jpg'),
+          // child: Image.NetworkImage('assets/space1.jpg'),
+          // image: NetworkImage('https://images.unsplash.com/photo-1531306728370-e2ebd9d7bb99?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+        ),
+
+        // child: Text(
+        //   "Hello Ninjas",
+        //   style: TextStyle(
+        //     fontSize: 20.0,
+        //     fontWeight: FontWeight.bold,
+        //     letterSpacing: 2.0,
+        //     color: Colors.grey,
+        //     fontFamily: 'IndieFlower',
+        //   ),
+        // ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
+        backgroundColor: Colors.green[600],
         child: const Text("Click"),
       ),
-    ),
-  ));
+    );
+  }
 }
